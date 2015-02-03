a048ecdd39ac25abbb5793245d75060d5683088c
Stjerndal <stjernd@kth.se>
2015-02-03 02:03:18 +0100

    Creating Pickup class.
    Cpp & header file.

--- UE4ProgIntro/Source/UE4ProgIntro/Private/Pickup.cpp
+++ UE4ProgIntro/Source/UE4ProgIntro/Private/Pickup.cpp
@@ -4,5 +4,31 @@
 #include "Pickup.h"
 
 
+APickup::APickup(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	// The pickup is valid when it is created.
+	bIsActive = true;
 
+	// Create the root SphereComponent to handle the pickup's collision
+	BaseCollisionComponent = ObjectInitializer.CreateDefaultSubobject<USphereComponent>(this, TEXT("BaseSphereComponent"));
+
+	// Set the SphereComponent as the root component.
+	RootComponent = BaseCollisionComponent;
+
+	//Create the static mesh component
+	PickupMesh = ObjectInitializer.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("PickupMesh"));
+
+	// Turn physics on for the static mesh.
+	PickupMesh->SetSimulatePhysics(true);
+
+	//attach the static mesh component to the root component
+	PickupMesh->AttachTo(RootComponent);
+
+}
+
+
+void APickup::OnPickedUp_Implementation()	//<--this is a Blueprint Native event, so we need to add _Implementation
+{
+	//there is no default behavior for a pickup when its picked up.
+}
 
--- UE4ProgIntro/Source/UE4ProgIntro/Public/Pickup.h
+++ UE4ProgIntro/Source/UE4ProgIntro/Public/Pickup.h
@@ -13,7 +13,23 @@ class UE4PROGINTRO_API APickup : public AActor
 {
 	GENERATED_BODY()
 		
+public:
+
+	APickup(const FObjectInitializer& ObjectInitializer);
+
+	//True when the pickup is able to be picked up, false if something deactivates the pickup.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool bIsActive;
 	
-	
-	
+	//Simple collision primitive to use as the root component.
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	class USphereComponent* BaseCollisionComponent;
+
+	//StaticMeshComponent to represent the pickup in the level.
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	class UStaticMeshComponent* PickupMesh;
+
+	//Function to call when the Pickup is collected.
+	UFUNCTION(BlueprintNativeEvent)
+		void OnPickedUp();
 };
